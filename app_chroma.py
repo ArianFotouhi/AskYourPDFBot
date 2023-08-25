a64205f42fd9afe0b7e33e16675f26ac8031f4d5
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-08-25 14:59:54 -0400

    v1.8

---
+++ app_chroma.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+
+os.environ["OPENAI_API_KEY"] = ""
+
+# load the document as before
+loader = PyPDFLoader('Document.pdf')
+documents = loader.load()
+
+# we split the data into chunks of 1,000 characters, with an overlap
+# of 200 characters between the chunks, which helps to give better results
+# and contain the context of the information between chunks
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+documents = text_splitter.split_documents(documents)
+
+# we create our vectorDB, using the OpenAIEmbeddings tranformer to create
+# embeddings from our text chunks. We set all the db information to be stored
+# inside the ./data directory, so it doesn't clutter up our source files
+vectordb = Chroma.from_documents(
+  documents,
+  embedding=OpenAIEmbeddings(),
+  persist_directory='./data'
+)
+vectordb.persist()
+
+pdf_qa = ConversationalRetrievalChain.from_llm(
+    ChatOpenAI(temperature=0.9, model_name="gpt-3.5-turbo"),
+    vectordb.as_retriever(search_kwargs={'k': 6}),
+    return_source_documents=True,
+    verbose=False
+)
+
+yellow = "\033[0;33m"
+green = "\033[0;32m"
+white = "\033[0;39m"
+
+chat_history = []
+print(f"{yellow}---------------------------------------------------------------------------------")
+print('Welcome to the DocBot. You are now ready to start interacting with your documents')
+print('---------------------------------------------------------------------------------')
+while True:
+    query = input(f"{green}Prompt: ")
+    if query == "exit" or query == "quit" or query == "q" or query == "f":
+        print('Exiting')
+        sys.exit()
+    if query == '':
+        continue
+    result = pdf_qa(
+        {"question": query, "chat_history": chat_history})
+    print(f"{white}Answer: " + result["answer"])
+    chat_history.append((query, result["answer"]))
